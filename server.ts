702b34201f136eba88bfe476ff158a215eabe1f8
Bunts <dubbzi321@gmail.com>
2025-05-16 08:02:57 +0300

    enabled CORS

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
-// import cors from 'cors';
+import cors from 'cors';
 import { connectDB } from './config/db';
 import userRoutes from './routes/user.routes';
 
@@ -10,13 +10,14 @@ import userRoutes from './routes/user.routes';
 const app = express();
 connectDB();
 
-app.get('/', (req, res) => {
-    res.status(200).json({ status: 'OK', message: 'Server is running' });
-  });
-
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
+
 app.use('/api/auth', userRoutes);
+
+app.get('/', (req, res) => {
+  res.status(200).json({ status: 'OK', message: 'Server is running' });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
